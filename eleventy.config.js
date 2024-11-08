e9b841108bdddfa8f2ab086b07d6e3b562fa9fcb
riotgoools <91990710+riotgoools@users.noreply.github.com>
2024-11-08 16:22:03 -0700

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -36,4 +36,7 @@ module.exports = function(eleventyConfig) {
 		console.log('[pagefind] Creating search index.');
 		execSync(`npx pagefind --site _site --glob \"[0-9]*/**/*.html\"`, { encoding: 'utf-8' });
   });
+
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+    return {pathPrefix: "/twitter/"}
 };
