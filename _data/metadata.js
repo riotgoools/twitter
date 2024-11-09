90902657becbcb85b3784e936437ca5dfdb6212a
riotgoools <91990710+riotgoools@users.noreply.github.com>
2024-11-08 19:58:38 -0700

    site upload

--- _data/metadata.js
+++
@@ -1,9 +0,0 @@
-let data = {
-	username: "riotgoools", // No leading @ here
-	homeLabel: "riotgoools.com",
-	homeUrl: "https://riotgoools.com/",
-};
-
-data.avatar = `https://gravatar.com/userimage/254370861/0adae649fb5bc711530e7605e6913ef5.jpeg?size=256&cache=1731096987973`;
-
-module.exports = data;
